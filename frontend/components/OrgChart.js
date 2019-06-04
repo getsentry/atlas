698c2e4f690acaae0ebe401f2547470f6e0fb706
David Cramer <dcramer@gmail.com>
2019-06-03 21:16:31 -0700

    Janky org chart based on react-orgchart

---
+++ frontend/components/OrgChart.js
@@ -0,0 +1,82 @@
+import React from "react";
+
+export default ({ tree, NodeComponent }) => {
+  const renderChildren = node => {
+    const hasSiblingRight = childIndex => {
+      return (node.children || []).length > childIndex + 1;
+    };
+
+    const hasSiblingLeft = childIndex => {
+      return childIndex > 0;
+    };
+
+    const nodeLineBelow = (
+      <td
+        colSpan={(node.children || []).length * 2}
+        className="nodeGroupCellLines"
+      >
+        <table className="nodeLineTable">
+          <tbody>
+            <tr>
+              <td
+                colSpan={2}
+                className="nodeLineCell nodeGroupLineVerticalMiddle"
+              />
+              <td colSpan={2} className="nodeLineCell" />
+            </tr>
+          </tbody>
+        </table>
+      </td>
+    );
+
+    const childrenLinesAbove = (node.children || []).map(
+      (child, childIndex) => (
+        <td colSpan="2" className="nodeGroupCellLines" key={childIndex}>
+          <table className="nodeLineTable">
+            <tbody>
+              <tr>
+                <td
+                  colSpan={2}
+                  className={
+                    "nodeLineCell nodeGroupLineVerticalMiddle" +
+                    (hasSiblingLeft(childIndex) ? " nodeLineBorderTop" : "")
+                  }
+                />
+                <td
+                  colSpan={2}
+                  className={
+                    "nodeLineCell" +
+                    (hasSiblingRight(childIndex) ? " nodeLineBorderTop" : "")
+                  }
+                />
+              </tr>
+            </tbody>
+          </table>
+        </td>
+      )
+    );
+
+    const children = (node.children || []).map((child, childIndex) => (
+      <td colSpan="2" className="nodeGroupCell" key={childIndex}>
+        {renderChildren(child)}
+      </td>
+    ));
+
+    return (
+      <table className="orgNodeChildGroup" key={node.id}>
+        <tbody>
+          <tr>
+            <td className="nodeCell" colSpan={(node.children || []).length * 2}>
+              <NodeComponent node={node} />
+            </td>
+          </tr>
+          <tr>{(node.children || []).length > 0 && nodeLineBelow}</tr>
+          <tr>{childrenLinesAbove}</tr>
+          <tr>{children}</tr>
+        </tbody>
+      </table>
+    );
+  };
+
+  return <div className="reactOrgChart">{tree.map(renderChildren)}</div>;
+};
---
+++ frontend/pages/orgChart.js
@@ -0,0 +1,126 @@
+import React from "react";
+import { Query } from "react-apollo";
+import gql from "graphql-tag";
+
+import OrgChart from "../components/OrgChart";
+import Layout from "../components/Layout";
+
+export const LIST_ALL_PEOPLE_QUERY = gql`
+  query listAllPeople {
+    users(offset: 0, limit: 1000) {
+      id
+      name
+      profile {
+        title
+        reportsTo {
+          id
+          name
+          profile {
+            title
+          }
+        }
+      }
+    }
+  }
+`;
+
+const listToTree = list => {
+  var map = {},
+    node,
+    roots = [],
+    i;
+  for (i = 0; i < list.length; i += 1) {
+    map[list[i].id] = i; // initialize the map
+    list[i].children = []; // initialize the children
+  }
+  for (i = 0; i < list.length; i += 1) {
+    node = list[i];
+    if (node.profile && node.profile.reportsTo) {
+      // if you have dangling branches check that map[node.parentId] exists
+      list[map[node.profile.reportsTo.id]].children.push(node);
+    } else {
+      roots.push(node);
+    }
+  }
+  return roots;
+};
+
+const Node = ({ node }) => {
+  return <div>{node.name}</div>;
+};
+
+export default () => (
+  <Layout>
+    <h1>Org Chart</h1>
+    <style global jsx>{`
+      .reactOrgChart {
+        margin: 2px;
+      }
+
+      .reactOrgChart .orgNodeChildGroup .node {
+        border: solid 1px #000000;
+        display: inline-block;
+        padding: 4px;
+        width: 100px;
+      }
+
+      .reactOrgChart .orgNodeChildGroup .nodeCell {
+        border: solid 3px red;
+        border-radius: 3px;
+        padding: 5px;
+        width: 150px;
+        display: inline-block;
+        text-align: center;
+      }
+
+      .reactOrgChart .orgNodeChildGroup .nodeGroupCell {
+        vertical-align: top;
+      }
+
+      .reactOrgChart .orgNodeChildGroup .nodeGroupLineVerticalMiddle {
+        height: 25px;
+        width: 50%;
+        border-right: solid 3px red;
+      }
+
+      .reactOrgChart .nodeLineBorderTop {
+        border-top: solid 3px red;
+      }
+
+      .reactOrgChart table {
+        border-collapse: collapse;
+        border: none;
+        margin: 0 auto;
+      }
+
+      .reactOrgChart td {
+        padding: 0;
+      }
+
+      .reactOrgChart table.nodeLineTable {
+        width: 100%;
+      }
+
+      .reactOrgChart table td.nodeCell {
+        width: 50%;
+      }
+    `}</style>
+    <Query query={LIST_ALL_PEOPLE_QUERY}>
+      {({ loading, error, data, fetchMore }) => {
+        if (error) return <ErrorMessage message="Error loading people." />;
+        if (loading) return <div>Loading</div>;
+        const { users } = data;
+        users.sort((a, b) => {
+          if (!!a.reportsTo && !!b.reportsTo) {
+            return 0;
+          } else if (!!a.reportsTo) {
+            return 1;
+          }
+          return -1;
+        });
+        const dataSource = listToTree(users);
+        return <OrgChart tree={dataSource} NodeComponent={Node} />;
+      }}
+    </Query>
+  </Layout>
+);
