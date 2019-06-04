330309fe73f5c6f4d729dc795a020c8f625e4886
David Cramer <dcramer@gmail.com>
2019-06-03 18:27:51 -0700

    Error hard when missing Google config for server

--- backend/apps.py
+++ backend/apps.py
@@ -1,4 +1,6 @@
 from django.apps import AppConfig
+from django.conf import settings
+from django.core.exceptions import ImproperlyConfigured
 
 
 class AppConfig(AppConfig):
@@ -6,4 +8,7 @@ class AppConfig(AppConfig):
     verbose_name = "atlas-backend"
 
     def ready(self):
-        pass
+        if not settings.GOOGLE_CLIENT_ID:
+            raise ImproperlyConfigured("You have not configured GOOGLE_CLIENT_ID.")
+        if not settings.GOOGLE_CLIENT_SECRET:
+            raise ImproperlyConfigured("You have not configured GOOGLE_CLIENT_SECRET.")
