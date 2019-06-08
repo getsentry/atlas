a8fca54523e9785ec2e240a49a46c5fb70dca5de
David Cramer <dcramer@gmail.com>
2019-06-08 13:23:12 -0700

    Require user modification scope

--- frontend/config.js
+++ frontend/config.js
@@ -4,6 +4,6 @@ module.exports = {
   GOOGLE_CLIENT_ID:
     "514555689588-uj2mgn0cbno4dmbib9m6oc9nfsubkp27.apps.googleusercontent.com",
   GOOGLE_SCOPES:
-    "https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/admin.directory.user.readonly",
+    "https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/admin.directory.user",
   GOOGLE_REDIRECT_URI: "http://localhost:8080"
 };
