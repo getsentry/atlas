1f333deddb59c0cc614b9ea7edeb98f8c00739ac
David Cramer <dcramer@gmail.com>
2019-06-08 12:55:24 -0700

    Initial org chart mocks

--- backend/atlas/factories/profile.py
+++ backend/atlas/factories/profile.py
@@ -1,3 +1,4 @@
+import itertools
 from datetime import date
 
 import factory
@@ -6,11 +7,62 @@ import factory.fuzzy
 from .. import models
 from .user import UserFactory
 
+DEPARTMENTS = [
+    "Engineering",
+    "Product",
+    "Customer Support",
+    "Customer Success",
+    "People",
+    "Sales",
+    "Marketing",
+    "Finance",
+    "Legal",
+    "Design",
+]
+
+DEPARTMENT_TITLES = {
+    "Engineering": ["Software Engineer"],
+    "Product": ["Product Manager"],
+    "Customer Support": ["Customer Support Manager"],
+    "Customer Success": ["Customer Success Manager"],
+    "People": ["People Operations Manager", "HR Specialist", "Recruiter"],
+    "Marketing": [
+        "Product Marketing Manager",
+        "Content Strategist",
+        "Marketing Operations Manager",
+        "Events Coordinator",
+    ],
+    "Sales": [
+        "Account Executive",
+        "Sales Engineer",
+        "Sales Development Representative",
+        "Sales Operations Manager",
+    ],
+    "Finance": ["Staff Accountant"],
+    "Legal": ["Counsel"],
+    "Design": ["Product Designer", "Illustrator"],
+}
+
+TITLES = ["Software Engineer", "Product Manager", "Customer Success Manager", ""]
+
 
 class ProfileFactory(factory.django.DjangoModelFactory):
     user = factory.SubFactory(UserFactory)
-    title = factory.Faker("name")
+    title = factory.Iterator(TITLES)
+    department = factory.LazyAttribute(lambda o: itertools.choice(DEPARTMENTS[o.title]))
     date_started = factory.fuzzy.FuzzyDate(start_date=date(2010, 1, 1))
 
     class Meta:
         model = models.Profile
+
+    class Params:
+        ceo = factory.Trait(title="Chief Executive Officer", department="G&A")
+        cfo = factory.Trait(title="Chief Financial Officer", department="G&A")
+        cpo = factory.Trait(title="Chief Product Officer", department="G&A")
+        cto = factory.Trait(title="Chief Technology Officer", department="G&A")
+        cmo = factory.Trait(title="Chief Marketing Officer", department="G&A")
+        cro = factory.Trait(title="Chief Revenue Officer", department="G&A")
+        marketing = factory.Trait(department="Marketing")
+        engineering = factory.Trait(department="Engineering")
+        sales = factory.Trait(department="Sales")
+        finance = factory.Trait(department="Finance")
---
+++ backend/atlas/management/commands/load_mocks.py
@@ -0,0 +1,23 @@
+from django.core.management.base import BaseCommand
+
+from atlas import factories
+
+
+class Command(BaseCommand):
+    help = "Load mock data"
+
+    def handle(self, *args, **options):
+        ceo = factories.ProfileFactory.create(ceo=True)
+        print(f"Created {ceo}")
+        cmo = factories.ProfileFactory.create(cmo=True, reports_to=ceo.user)
+        print(f"Created {cmo}")
+        cto = factories.ProfileFactory.create(cto=True, reports_to=ceo.user)
+        print(f"Created {cto}")
+        cpo = factories.ProfileFactory.create(cpo=True, reports_to=ceo.user)
+        print(f"Created {cpo}")
+        cfo = factories.ProfileFactory.create(cfo=True, reports_to=ceo.user)
+        print(f"Created {cfo}")
+        cro = factories.ProfileFactory.create(cfo=True, reports_to=ceo.user)
+        print(f"Created {cro}")
+
+        factories.ProfileFactory.create(reports_to=cmo.user, marketing=True)
