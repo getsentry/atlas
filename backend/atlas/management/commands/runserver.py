a471fc5d362371ad0f9ad698c9db099486044f35
David Cramer <dcramer@gmail.com>
2019-06-08 12:55:07 -0700

    Fix runserver without staticfiles

--- backend/atlas/management/commands/runserver.py
+++ backend/atlas/management/commands/runserver.py
@@ -1,7 +1,11 @@
 from django.conf import settings
-from django.contrib.staticfiles.management.commands.runserver import (
-    Command as BaseCommand,
-)
+
+if "django.contrib.staticfiles" in settings.INSTALLED_APPS:
+    from django.contrib.staticfiles.management.commands.runserver import (
+        Command as BaseCommand,
+    )
+else:
+    from django.core.management.commands.runserver import Command as BaseCommand
 
 
 class Command(BaseCommand):
