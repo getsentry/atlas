d585278322a6cf02e9aabcd0755efbfa61c2d543
David Cramer <dcramer@gmail.com>
2019-06-10 15:15:48 -0700

    Expose concurrency option

--- backend/atlas/management/commands/worker.py
+++ backend/atlas/management/commands/worker.py
@@ -12,16 +12,19 @@ class Command(BaseCommand):
             "--no-cron", dest="cron", action="store_false", default=True
         )
         parser.add_argument("--log-level", dest="log_level", default="INFO")
+        parser.add_argument("--concurrency", "-c", dest="concurrency", default=None)
 
-    def handle(self, cron=True, log_level="INFO", **options):
+    def handle(self, cron, log_level, concurrency, **options):
         command = [
             "celery",
             "--app=atlas.celery:app",
             "worker",
-            "--loglevel={}".format(log_level),
+            f"--loglevel={log_level}",
             "--max-tasks-per-child=10000",
         ]
+        if concurrency:
+            command.append(f"--concurrency={concurrency}")
         if cron:
-            command.extend(["--beat"])
+            command.append("--beat")
 
         os.execvp(command[0], command)
