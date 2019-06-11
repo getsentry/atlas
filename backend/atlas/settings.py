3acadf94aa1f7642dc619526967e5436e44d3a5d
David Cramer <dcramer@gmail.com>
2019-06-10 21:11:53 -0700

    Allow SECRET_KEY via env

--- backend/atlas/settings.py
+++ backend/atlas/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "a weak ass secret key"
+SECRET_KEY = os.environ.get("SECRET_KEY") or "a weak ass secret key"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", False)
