6239d2e8b5b864e74e9a33a2d27450eb1894fa26
David Cramer <dcramer@gmail.com>
2019-08-07 15:53:37 -0700

    Enable tracing for frontend

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,7 @@ import { Router, browserHistory } from "react-router";
 import { render } from "react-dom";
 import { Provider } from "react-redux";
 import * as Sentry from "@sentry/browser";
+import { Tracing } from "@sentry/integrations";
 import { ApolloProvider } from "react-apollo";
 
 import routes from "./routes";
@@ -14,7 +15,12 @@ import apolloClient from "./utils/apollo";
 Sentry.init({
   dsn: config.sentryDsn,
   environment: config.environment,
-  release: config.version
+  release: config.version,
+  integrations: [
+    new Tracing({
+      tracingOrigins: ["localhost", "atlas.getsentry.net", /^\//]
+    })
+  ]
 });
 
 // If you want your app to work offline and load faster, you can change
