0a92089946fc87ab539a0500b85466860157a790
David Cramer <dcramer@gmail.com>
2019-08-08 08:56:20 -0700

    Blowing up the spans with all nodes

--- backend/atlas/utils/graphql.py
+++ backend/atlas/utils/graphql.py
@@ -6,15 +6,14 @@ import sentry_sdk
 class TracingMiddleware(object):
     def _after_resolve(self, span, info, data):
         span.__exit__(None, None, None)
-        span.description = str(info.field_name)
+        span.description = ".".join(info.path)
         return data
 
     def resolve(self, _next, root, info, *args, **kwargs):
-        if len(info.path) == 1:
-            span = sentry_sdk.Hub.current.span(
-                transaction=info.path[0], op=info.operation.operation
-            )
-            span.__enter__()
-            on_result_f = partial(self._after_resolve, span, info)
-            return _next(root, info, *args, **kwargs).then(on_result_f)
-        return _next(root, info, *args, **kwargs)
+        span = sentry_sdk.Hub.current.span(
+            transaction=info.path[0] if len(info.path) == 1 else None,
+            op=info.operation.operation,
+        )
+        span.__enter__()
+        on_result_f = partial(self._after_resolve, span, info)
+        return _next(root, info, *args, **kwargs).then(on_result_f)
