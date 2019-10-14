2a4488fa6916bab7e20f8713903f6531eaa2fcf3
David Cramer <dcramer@gmail.com>
2019-10-14 10:22:59 -0700

    Super high quality powerful organization chart

--- frontend/src/components/OrgChart.js
+++ frontend/src/components/OrgChart.js
@@ -1,10 +1,19 @@
 import React, { Component } from "react";
-import Draggable from "react-draggable";
 import styled from "@emotion/styled";
 
-import colors from "../colors";
+const ChildSet = styled.ul`
+  margin: 0;
+`;
 
-class OrgChart extends Component {
+const Node = styled.div`
+  border: solid 1px hsla(0, 0%, 100%, 0.1);
+  display: inline-block;
+  padding: 4px;
+  width: 250px;
+  margin-bottom: 0.25rem;
+`;
+
+export default class OrgChart extends Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -20,171 +29,39 @@ class OrgChart extends Component {
     this.setState({ zoom: this.state.zoom - 0.05 });
   };
 
+  renderNode = node => {
+    return (
+      <Node>
+        <this.props.NodeComponent node={node} />
+      </Node>
+    );
+  };
+
+  renderChildren = children => {
+    if (!children.length) return null;
+    return (
+      <ChildSet>
+        {children.map(child => {
+          return (
+            <li key={child.id}>
+              {this.renderNode(child)}
+              {this.renderChildren(child.children)}
+            </li>
+          );
+        })}
+      </ChildSet>
+    );
+  };
+
   render() {
-    const { tree, NodeComponent } = this.props;
-    const renderChildren = node => {
-      const hasSiblingRight = childIndex => {
-        return (node.children || []).length > childIndex + 1;
-      };
-
-      const hasSiblingLeft = childIndex => {
-        return childIndex > 0;
-      };
-
-      const nodeLineBelow = (
-        <td colSpan={(node.children || []).length * 2} className="nodeGroupCellLines">
-          <table className="nodeLineTable">
-            <tbody>
-              <tr>
-                <td colSpan={2} className="nodeLineCell nodeGroupLineVerticalMiddle" />
-                <td colSpan={2} className="nodeLineCell" />
-              </tr>
-            </tbody>
-          </table>
-        </td>
-      );
-
-      const childrenLinesAbove = (node.children || []).map((child, childIndex) => (
-        <td colSpan="2" className="nodeGroupCellLines" key={childIndex}>
-          <table className="nodeLineTable">
-            <tbody>
-              <tr>
-                <td
-                  colSpan={2}
-                  className={
-                    "nodeLineCell nodeGroupLineVerticalMiddle" +
-                    (hasSiblingLeft(childIndex) ? " nodeLineBorderTop" : "")
-                  }
-                />
-                <td
-                  colSpan={2}
-                  className={
-                    "nodeLineCell" +
-                    (hasSiblingRight(childIndex) ? " nodeLineBorderTop" : "")
-                  }
-                />
-              </tr>
-            </tbody>
-          </table>
-        </td>
-      ));
-
-      const children = (node.children || []).map((child, childIndex) => (
-        <td colSpan="2" className="nodeGroupCell" key={childIndex}>
-          {renderChildren(child)}
-        </td>
-      ));
-
-      return (
-        <table className="orgNodeChildGroup" key={node.id}>
-          <tbody>
-            <tr>
-              <td className="nodeCell" colSpan={(node.children || []).length * 2}>
-                <NodeComponent node={node} />
-              </td>
-            </tr>
-            <tr>{(node.children || []).length > 0 && nodeLineBelow}</tr>
-            <tr>{childrenLinesAbove}</tr>
-            <tr>{children}</tr>
-          </tbody>
-        </table>
-      );
-    };
+    const { tree } = this.props;
 
     return (
       <div className={this.props.className}>
         <div className="reactOrgChart" style={{ zoom: this.state.zoom }}>
-          <Draggable>
-            <div>{tree.map(renderChildren)}</div>
-          </Draggable>
+          {this.renderChildren(tree)}
         </div>
       </div>
     );
   }
 }
-
-const StyledOrgChart = styled(OrgChart)`
-  position: absolute;
-  left: 0.75rem;
-  right: 0.75rem;
-  bottom: 0;
-  top: 34px;
-  zoom: 0.9;
-  margin-top: 3rem;
-  background-color: ${colors.background};
-  border-top: 1px solid hsla(0, 0%, 100%, 0.1);
-  display: flex;
-  flex: 1;
-  background-size: 10px 10px;
-  background-image: linear-gradient(to right, rgba(1, 1, 1, 0.03) 1px, transparent 1px),
-    linear-gradient(to bottom, rgba(1, 1, 1, 0.03) 1px, transparent 1px);
-
-  .reactOrgChart {
-    margin: 2px;
-    padding: 2rem;
-    display: block;
-    overflow: hidden;
-    cursor: all-scroll;
-    flex-grow: 1;
-  }
-
-  .reactOrgChart .orgNodeChildGroup .node {
-    border: solid 1px hsla(0, 0%, 100%, 0.1);
-    display: inline-block;
-    padding: 4px;
-    width: 100px;
-  }
-
-  .reactOrgChart .orgNodeChildGroup .nodeCell {
-    text-align: center;
-  }
-
-  .reactOrgChart .orgNodeChildGroup .nodeCell .nodeItem {
-    border: solid 1px hsla(0, 0%, 100%, 0.1);
-    margin: 0 3px;
-    border-radius: 3px;
-    padding: 5px;
-    width: 200px;
-    display: inline-block;
-    color: ${colors.cardText};}
-    background: ${colors.background};
-  }
-  .reactOrgChart .orgNodeChildGroup .nodeCell .nodeItem img {
-    display: inline-block;
-    width: 32px;
-  }
-
-  .reactOrgChart .orgNodeChildGroup .nodeGroupCell {
-    vertical-align: top;
-  }
-
-  .reactOrgChart .orgNodeChildGroup .nodeGroupLineVerticalMiddle {
-    height: 25px;
-    width: 50%;
-    border-right: solid 1px hsla(0, 0%, 100%, 0.1);
-  }
-
-  .reactOrgChart .nodeLineBorderTop {
-    border-top: solid 1px hsla(0, 0%, 100%, 0.1);
-  }
-
-  .reactOrgChart table {
-    border-collapse: collapse;
-    border: none;
-    margin: 0 auto;
-  }
-
-  .reactOrgChart td {
-    padding: 0;
-  }
-
-  .reactOrgChart table.nodeLineTable {
-    width: 100%;
-  }
-
-  .reactOrgChart table td.nodeCell {
-    width: 50%;
-  }
-`;
-
-export default StyledOrgChart;
