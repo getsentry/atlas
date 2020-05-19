5c6c155742e84ec7710e18f06d7fefbea9dcd914
David Cramer <dcramer@gmail.com>
2020-05-19 13:26:29 -0700

    Fix changes query test

--- backend/atlas/queries/test_changes.py
+++ backend/atlas/queries/test_changes.py
@@ -1,13 +1,14 @@
 from atlas.models import Change
 
 
-def test_changes(gql_client, default_user, default_office):
+def test_changes(gql_client, default_superuser, default_office):
     change = Change.objects.create(
         object_type="user",
-        object_id=default_user.id,
+        object_id=default_superuser.id,
         changes={"name": "Joe Dirt"},
-        user=default_user,
+        user=default_superuser,
+        version=1,
     )
 
-    executed = gql_client.execute("""{changes {id}}""", user=default_user)
+    executed = gql_client.execute("""{changes {id}}""", user=default_superuser)
     assert executed["data"]["changes"] == [{"id": str(change.id)}]
