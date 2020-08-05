619a214af02b5296f684792cf93eefb8d1e9ea17
Scott Cooper <scttcper@gmail.com>
2020-08-04 18:00:10 -0700

    feat: add interactive org chart

---
+++ frontend/src/pages/OrgChartInteractive.js
@@ -0,0 +1,68 @@
+import React from "react";
+import { Query } from "react-apollo";
+import gql from "graphql-tag";
+import { OrgChart } from "@ctrl/react-orgchart";
+import { arrayToTree } from "performant-array-to-tree";
+import styled from "@emotion/styled";
+
+import colors from "../colors";
+import Layout from "../components/Layout";
+import PageLoader from "../components/PageLoader";
+import avatarPersonnel from "../images/avatar.svg";
+
+export const LIST_ALL_PEOPLE_QUERY = gql`
+  query listAllPeople {
+    users(offset: 0, limit: 1000, titlesOnly: true, humansOnly: true) {
+      results {
+        id
+        name
+        email
+        title
+        photo {
+          data
+          width
+          height
+          mimeType
+        }
+        reportsTo {
+          id
+        }
+      }
+    }
+  }
+`;
+
+export default () => (
+  <Layout>
+    <Query query={LIST_ALL_PEOPLE_QUERY}>
+      {({ loading, error, data }) => {
+        if (error) throw error;
+        if (loading) return <PageLoader />;
+        const { users } = data;
+        const tree = arrayToTree(
+          users.results.map(x => ({
+            ...x,
+            entity: {
+              ...x,
+              avatar:
+                (x.photo && x.photo.data && `data:image/jpeg;base64,${x.photo.data}`) ||
+                avatarPersonnel
+            },
+            parentId: x.reportsTo && x.reportsTo.id
+          })),
+          { dataField: null }
+        );
+        return (
+          <Container>
+            <OrgChart tree={tree[0]} borderColor={colors.primary400} />
+          </Container>
+        );
+      }}
+    </Query>
+  </Layout>
+);
+
+const Container = styled.div`
+  height: calc(100vh - 200px);
+  width: 100vw;
+`;
